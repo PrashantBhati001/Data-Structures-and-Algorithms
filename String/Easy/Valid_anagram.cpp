20f5ce0549e94bdba8790ef8edb8bd069865e3e4
Prashant Bhati <prashantbsmvit070@gmail.com>
2024-02-10 14:55:16 +0530

    commit

--- String/Easy/Valid_anagram.cpp
+++ String/Easy/Valid_anagram.cpp
@@ -4,7 +4,30 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
 
-        //Approach-2
+        //Approach-1:
+        vector<int> v(26,0);
+        for(int i=0;i<s.size();i++)
+        {
+            int pos=s[i]-'a';
+            v[pos]++;
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            int pos=t[i]-'a';
+            v[pos]--;
+
+        }
+
+        for(int i=0;i<26;i++)
+        {
+            if(v[i]!=0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+        //Approach-2:
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
         if(s==t)
