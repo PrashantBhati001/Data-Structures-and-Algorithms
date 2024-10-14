a3cc45dd32a7dc38acf78ae757c75c8178c6e92c
Prashant Bhati <prashantbsmvit070@gmail.com>
2024-10-14 08:30:42 +0530

    commit

--- Searching and Sorting/Easy/Kth_missing_positive.cpp
+++ Searching and Sorting/Easy/Kth_missing_positive.cpp
@@ -3,6 +3,45 @@ using namespace std;
 
 class Solution {
 public:
+
+  //O(n) approach
+    //   int findKthPositive(vector<int>& arr, int k) {
+
+    //     int n=arr.size();
+    //     int num=1;
+    //     int i=0;
+    //     while(i<n)
+    //     {
+    //         if(arr[i]==num)
+    //         {
+    //             i++;
+    //             num++;
+    //         }
+    //         else
+    //         {
+    //             k--;
+    //             if(k==0)
+    //             {
+    //                 return num;
+    //             }
+
+    //             num++;
+    //         }
+
+    //     }
+
+    //     while(k)
+    //     {
+    //         num++;
+    //         k--;
+
+    //     }
+
+    //     return num-1;
+  
+        
+    // }
+
     int findKthPositive(vector<int>& arr, int k) {
 
         int n=arr.size();
